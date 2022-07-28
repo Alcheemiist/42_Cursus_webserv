4d75f2d6a0e2b0673c143f3f6b6af227a8883178
chegashi <mochegri@student.1337.ma>
2022-07-28 17:22:15 +0100

    restract class

--- repo/networking/response/Response.cpp
+++ repo/networking/response/Response.cpp
@@ -2,26 +2,136 @@
 #include <fcntl.h>
 #include <sys/stat.h>
 
-std::vector<std::string> init_status_code()
+Response::Response() : version("HTTP/1.1 "), status("200 OK\r\n"), header(""),
+    body(""), response(""), responseStatus(""), body_length(0), contentType(""){};
+
+void Response::setVersion(std::string version)
 {
-	std::vector<std::string> status_vector;
-    status_vector.push_back("200 OK");
-    status_vector.push_back("204 NO CONTENT");
-    status_vector.push_back("301 MOVED PERMANENTLY");
-    status_vector.push_back("400 BAD REQUEST");
-    status_vector.push_back("403 FORBIDDEN");
-    status_vector.push_back("404 NOT FOUND");
-    status_vector.push_back("405 METHOD NOT ALLOWED");
-    status_vector.push_back("413 REQUEST ENTITY TOO LARGE");
-    status_vector.push_back("414 URI TOO LONG");
-    status_vector.push_back("500 INTERNAL SERVER ERROR");
-    status_vector.push_back("501 NOT IMPLEMENTED");
-    status_vector.push_back("502 BAD GATEWAY");
-    status_vector.push_back("503 SERVICE UNAVAILABLE");
-    status_vector.push_back("504 GATEWAY TIMEOUT");
-    status_vector.push_back("505 HTTP VERSION NOT SUPPORTED");
-	return status_vector;
+    this->version = version;
+    std::cout << "- Set Version : " << this->version << std::endl;
+};
+
+bool Response::url_parser(std::string url)
+{
+	std::string allowed = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-._~:/?#[]@!$&'()*+,;=";
+	for(std::string::iterator it = url.begin(); it != url.end(); ++it)
+		if (allowed.find(*it) == std::string::npos)
+			return false;
+	return (true);
 }
+
+void Response::setHeader(char *header)
+{
+    this->header = header;
+    std::cout << "- Set header : " << this->header << std::endl;
+};
+
+void Response::setBody(char *body)
+{
+    this->body = body;
+    std::cout << "- Set body of size = " << this->body.size() << std::endl;
+}
+
+void Response::setResponseStatus(char *status)
+{
+    this->responseStatus = status;
+    std::cout << "- Set Status : " << this->responseStatus << std::endl;
+};
+
+size_t Response::size() const { return body_length; };
+
+void Response::setResponseHeader()
+{
+    this->header = this->version.c_str();
+    std::cout << "- Set Version : " << this->version << std::endl;
+};
+
+std::string Response::getResponse() const
+{
+    std::string res;
+    res.append(version);
+    res.append(status);
+    // res.https://en.wikipedia.org/wiki/Plotting_algorithms_for_the_Mandelbrot_set("Content-Type: ");
+    // res.append(this->contentType);
+    // res.append("\r\n");
+    res.append("server: alchemist\r\n");
+    res.append("location: wonderland");
+    res.append("\r\n\r\n");
+    res.append(body);
+    if (res.size() < 1000)
+        std::cout << green << "{" << res << "}" << def << std::endl;
+    else
+        std::cout << green << "BUFFER SEND SIZE " << res.size() << def << std::endl;
+    return res;
+};
+
+void Response::setStatus(Request *request, parse_config *config)
+{
+	(void)request;
+	(void)config;
+    // this->status = status;
+	if (!request->get_is_formated())
+	{
+		if (request->get_transfer_encoding() != "chunked")
+			this->status = "501 NOT IMPLEMENTED";
+		else if (request->get_transfer_encoding() == "chunked" &&
+			request->getcontent_length() == -1)
+			this->status = "400 BAD REQUEST";
+		else if (url_parser(request->geturl()))
+			this->status = "400 BAD REQUEST";
+		else if (request->getcontent_length() > 2048)
+			this->status = "414 REQUEST-URI TOO LARGE";
+		else if (request->body_length() > request->getcontent_length())
+			this->status = "413 REQUEST ENTITY TOO LARGE";
+	}
+	// else
+	// {
+	// 	if (!url_macth())
+	// 		this->status = "400 BAD REQUEST";
+	// 	else if (url_have_redir())
+	// 		this->status = "301 MOVED PERMANENTLY";
+	// 	else if ((std::string request->get_method()))
+	// }
+    std::cout << "- Set Status : " << this->status << std::endl;
+}
+
+void Response::setContentType(char *path)
+{
+    std::string s(path);
+    std::string s1 = s.substr(s.find_last_of(".") + 1);
+    if (s1 == "html")
+        this->contentType = "text/html";
+    else if (s1 == "css")
+        this->contentType = "text/css";
+    else if (s1 == "js")
+        this->contentType = "application/javascript";
+    else if (s1 == "jpg")
+        this->contentType = "image/jpeg";
+    else if (s1 == "png")
+        this->contentType = "image/png";
+    else if (s1 == "gif")
+        this->contentType = "image/gif";
+    else if (s1 == "ico")
+        this->contentType = "image/x-icon";
+    else if (s1 == "svg")
+        this->contentType = "image/svg+xml";
+    else if (s1 == "mp3")
+        this->contentType = "audio/mpeg";
+    else if (s1 == "mp4")
+        this->contentType = "video/mp4";
+    else if (s1 == "ogg")
+        this->contentType = "audio/ogg";
+    else if (s1 == "ogv")
+        this->contentType = "video/ogg";
+    else if (s1 == "wav")
+        this->contentType = "audio/wav";
+    else if (s1 == "webm")
+        this->contentType = "video/webm";
+    else if (s1 == "txt")
+        this->contentType = "text/plain";
+    std::cout << red << "- Set Content-Type : " << s << green << " " << s1 << def << std::endl;
+};
+
 void ERRORresponse(Request *request, Response *response)
 {
     (void)request;
@@ -80,7 +190,7 @@ void GETresponse(Request *request, Response *response, parse_config *config, int
 void response(int new_socket, Request *request, parse_config *config, int index_server)
 {
     Response response;
-	response.setStatus_vector(init_status_code());
+	// response.setStatus_vector(init_status_code());
 	response.setStatus(request, config);
     std::cout << blue << "********** { Response } ***********************" << def << std::endl;
     if (!request->isGoodrequest())
--- repo/networking/response/Response.hpp
+++ repo/networking/response/Response.hpp
@@ -21,143 +21,18 @@ private:
     std::vector<std::string> status_vector;
 
 public:
-    Response() : version("HTTP/1.1 "), status("200 OK\r\n"), header(""), body(""), response(""), responseStatus(""), body_length(0), contentType("")
-    {
-
-    };
-
-    void setVersion(std::string version)
-    {
-        this->version = version;
-        std::cout << "- Set Version : " << this->version << std::endl;
-    };
-
-	void setStatus_vector(std::vector<std::string> vect)
-	{
-		this->status_vector = vect;
-	}
-
-    void setStatus(Request *request, parse_config *config)
-    {
-		(void)request;
-		(void)config;
-        // this->status = status;
-		if (!request->get_is_formated())
-		{
-			if (request->get_transfer_encoding() != "chunked")
-				this->status = "501 NOT IMPLEMENTED";
-			else if (request->get_transfer_encoding() == "chunked" &&
-				request->getcontent_length() == -1)
-				this->status = "400 BAD REQUEST";
-			else if (url_parser(request->geturl()))
-				this->status = "400 BAD REQUEST";
-			else if (request->getcontent_length() > 2048)
-				this->status = "414 REQUEST-URI TOO LARGE";
-			else if (request->body_length() > request->getcontent_length())
-				this->status = "413 REQUEST ENTITY TOO LARGE";
-		}
-		// else
-		// {
-		// 	if (!url_macth())
-		// 		this->status = "400 BAD REQUEST";
-		// 	else if (url_have_redir())
-		// 		this->status = "301 MOVED PERMANENTLY";
-		// 	else if ((std::string request->get_method()))
-		// }
-
-        std::cout << "- Set Status : " << this->status << std::endl;
-	}
-
-	bool url_parser(std::string url)
-	{
-		std::string allowed = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-._~:/?#[]@!$&'()*+,;=";
-		for(std::string::iterator it = url.begin(); it != url.end(); ++it)
-			if (allowed.find(*it) == std::string::npos)
-				return false;
-		return (true);
-	}
-
-    void setHeader(char *header)
-    {
-        this->header = header;
-        std::cout << "- Set header : " << this->header << std::endl;
-    };
-
-    void setBody(char *body)
-    {
-        this->body = body;
-        std::cout << "- Set body of size = " << this->body.size() << std::endl;
-    };
-    void setResponseStatus(char *status)
-    {
-        this->responseStatus = status;
-        std::cout << "- Set Status : " << this->responseStatus << std::endl;
-    };
-
-    void setResponseHeader()
-    {
-        this->header = this->version.c_str();
-        std::cout << "- Set Version : " << this->version << std::endl;
-    };
-    size_t size() const { return body_length; };
-    std::string getResponse() const
-    {
-        std::string res;
-        res.append(version);
-        res.append(status);
-        // res.https://en.wikipedia.org/wiki/Plotting_algorithms_for_the_Mandelbrot_set("Content-Type: ");
-        // res.append(this->contentType);
-        // res.append("\r\n");
-        res.append("server: alchemist\r\n");
-        res.append("location: wonderland");
-        res.append("\r\n\r\n");
-        res.append(body);
-        if (res.size() < 1000)
-            std::cout << green << "{" << res << "}" << def << std::endl;
-        else
-            std::cout << green << "BUFFER SEND SIZE " << res.size() << def << std::endl;
-        return res;
-    };
+    Response();
+    void setVersion(std::string version);
+    void setStatus(Request *request, parse_config *config);
+	bool url_parser(std::string url);
+    void setHeader(char *header);
+    void setBody(char *body);
+    void setResponseStatus(char *status);
+    void setResponseHeader();
+    size_t size() const;
+    std::string getResponse() const;
     std::string getBody() { return body; };
-
-    void setContentType(char *path)
-    {
-        std::string s(path);
-        std::string s1 = s.substr(s.find_last_of(".") + 1);
-
-        if (s1 == "html")
-            this->contentType = "text/html";
-        else if (s1 == "css")
-            this->contentType = "text/css";
-        else if (s1 == "js")
-            this->contentType = "application/javascript";
-        else if (s1 == "jpg")
-            this->contentType = "image/jpeg";
-        else if (s1 == "png")
-            this->contentType = "image/png";
-        else if (s1 == "gif")
-            this->contentType = "image/gif";
-        else if (s1 == "ico")
-            this->contentType = "image/x-icon";
-        else if (s1 == "svg")
-            this->contentType = "image/svg+xml";
-        else if (s1 == "mp3")
-            this->contentType = "audio/mpeg";
-        else if (s1 == "mp4")
-            this->contentType = "video/mp4";
-        else if (s1 == "ogg")
-            this->contentType = "audio/ogg";
-        else if (s1 == "ogv")
-            this->contentType = "video/ogg";
-        else if (s1 == "wav")
-            this->contentType = "audio/wav";
-        else if (s1 == "webm")
-            this->contentType = "video/webm";
-        else if (s1 == "txt")
-            this->contentType = "text/plain";
-
-        std::cout << red << "- Set Content-Type : " << s << green << " " << s1 << def << std::endl;
-    };
+    void setContentType(char *path);
 };
 
 void GETresponse(Request *request, Response *response, Config *config);
